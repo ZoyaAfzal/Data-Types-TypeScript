420300e47281143f5f66534912ac1d91e710b2b1
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-03-03 21:17:42 +0500

    Data types

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+"use strict";
+//DATA TYPES:
+//String--
+let firstname = "Kenneth";
+let lastname = "Eric";
+let message = "Hope you are doing good.";
+console.log(`Hello ${firstname} ${lastname}! ${message}`); //string
+//Numbers--
+let num1 = 5190;
+let num2 = 189.9;
+console.log(num1 + num2); //number
+console.log(num1 - num2); //number
+console.log(num1 * num2); //number
+console.log(num1 % num2); //number
+console.log(num1 / num2); //number
+console.log(num1 ** num2); //infinity
+//Boolean--
+let isImplemented = true;
+let isNotImplemented = false;
+console.log(isImplemented); //boolean
+console.log(isNotImplemented); //boolean
+//Undefined--
+let term;
+console.log(term); //undefined
+//Null--
+let value = null;
+console.log(value); //null
+//BigInt--
+const BigNumber = BigInt(900736785546788855566);
+console.log(BigNumber); //bigInt
---
+++ datatypes.ts
@@ -0,0 +1,45 @@
+//DATA TYPES:
+
+//String--
+let firstname:string = "Kenneth";
+let lastname:string = "Eric";
+let message:string = "Hope you are doing good.";
+
+console.log(`Hello ${firstname} ${lastname}! ${message}`); //string
+
+//Numbers--
+let num1:number = 5190;
+let num2:number = 189.9;
+
+console.log(num1 + num2); //number
+console.log(num1 - num2); //number
+console.log(num1 * num2); //number
+console.log(num1 % num2); //number
+console.log(num1 / num2); //number
+console.log(num1 ** num2); //infinity
+
+
+//Boolean--
+let isImplemented:boolean = true;
+let isNotImplemented:boolean = false;
+
+console.log(isImplemented);//boolean
+console.log(isNotImplemented);//boolean
+
+
+//Undefined--
+let term:undefined;
+
+console.log(term); //undefined
+
+
+//Null--
+let value:null = null;
+
+console.log(value); //null
+
+
+//BigInt--
+const BigNumber:bigint = BigInt(900736785546788855566);
+console.log(BigNumber); //bigInt
+
